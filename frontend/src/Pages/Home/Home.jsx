e583fbf6d0b878791cfbe342dfd11c60ba7ea51e
HarnivazSingh <harnivaz1710.be21@chitkara.edu.in>
2024-07-24 18:19:41 +0530

    Started working on the Frontend part of Tomato, my food delivery website and finished the navbar, header and home page

---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './Home.css';
+import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
+
+
+const Home= () => {
+
+    const [category,setCategory]=useState("All");
+
+
+    
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+    </div>
+  )
+}
+
+export default Home;
+
