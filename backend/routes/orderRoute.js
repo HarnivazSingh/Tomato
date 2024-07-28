5ee5a5708a2f99dc23d986a5b983cc99ef97ae96
HarnivazSingh <harnivaz1710.be21@chitkara.edu.in>
2024-07-28 16:56:38 +0530

    Integrated the Stripe payment gateway

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { placeOrder } from "../controllers/orderController.js"
+import authMiddleware from "../middleware/auth.js"
+
+
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place",authMiddleware,placeOrder);
+
+export {orderRouter}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import {foodRouter} from "./routes/foodRoute.js";
 import {userRouter} from "./routes/userRoute.js";
 import 'dotenv/config'
 import { cartRouter } from "./routes/cartRoute.js";
+import { orderRouter } from "./routes/orderRoute.js";
 
 //app config
 const app=express();
@@ -27,6 +28,7 @@ app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 
 app.listen(port,()=>{
